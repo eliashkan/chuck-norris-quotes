e1fc83cf69321a6197fb68cb3344bdd16eea7dca
Elias Khan <khan.elias@gmail.com>
2020-11-07 11:01:18 +0100

    Initial commit

---
+++ src/test/java/com/realdolmen/chucknorrisquotes/ChuckNorrisQuotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realdolmen.chucknorrisquotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorrisQuotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
